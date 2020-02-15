5b5760a711b936a509810b6470deb72a3ac36261
abhinay9955 <kumarabhinay599@gmail.com>
2020-02-15 15:49:57 +0530

    added

---
+++ app/src/main/java/com/example/schoolproject/Activities/PTAActivity.java
@@ -0,0 +1,18 @@
+package com.example.schoolproject.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.schoolproject.R;
+
+public class PTAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pta);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/schoolproject/AddChildActivity.java
@@ -0,0 +1,14 @@
+package com.example.schoolproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_child);
+    }
+}
