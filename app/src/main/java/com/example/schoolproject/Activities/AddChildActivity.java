2a989ce87a99f7ddd9495f3419b22c886f81a038
Nikhilranjan7352 <nikhilranjan7352@gmail.com>
2020-02-15 17:15:12 +0530

    sign out done

--- app/src/main/java/com/example/schoolproject/Activities/AddChildActivity.java
+++ app/src/main/java/com/example/schoolproject/Activities/AddChildActivity.java
@@ -3,14 +3,52 @@ package com.example.schoolproject.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.schoolproject.R;
 
 public class AddChildActivity extends AppCompatActivity {
+    private EditText childname,childparent,childcontact,childclass,childroll;
+    private Button registerchildbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_child2);
+        setContentView(R.layout.activity_add_child);
+        Initialise();
+        registerchildbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerchild();
+            }
+        });
+    }
+
+    private void registerchild()
+    {
+        if(TextUtils.isEmpty(childname.getText().toString()) || TextUtils.isEmpty(childclass.getText().toString()) || TextUtils.isEmpty(childcontact.getText().toString()) ||
+                TextUtils.isEmpty(childparent.getText().toString()) || TextUtils.isEmpty(childroll.getText().toString()))
+        {
+            Toast.makeText(this, "Fill the recquired information", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+
+        }
+    }
+
+    private void Initialise()
+    {
+        childname=(EditText)findViewById(R.id.addchild_name);
+        childclass=(EditText)findViewById(R.id.addchild_class);
+        childcontact=(EditText)findViewById(R.id.addchild_contact);
+        childparent=(EditText)findViewById(R.id.addchild_guardian);
+        childroll=(EditText)findViewById(R.id.addchild_roll);
+        registerchildbutton=(Button)findViewById(R.id.addchildbutton);
+
     }
 }
--- app/src/main/java/com/example/schoolproject/Activities/TutorActivity.java
+++ app/src/main/java/com/example/schoolproject/Activities/TutorActivity.java
@@ -1,25 +1,45 @@
 package com.example.schoolproject.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.schoolproject.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TutorActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor);
+        mAuth=FirebaseAuth.getInstance();
     }
 
-    public static class AddChildActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.tutormenu,menu);
+        return true;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_add_child);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId()==R.id.signouttutor)
+        {
+            mAuth.signOut();
+            Intent intent=new Intent(this,LoginActivity.class);
+            startActivity(intent);
+
         }
+
+
+        return true;
     }
 }
