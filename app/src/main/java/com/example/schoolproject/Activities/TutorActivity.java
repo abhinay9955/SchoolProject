1338aa2c61c8ce8b789ab5b60648441707699fee
abhinay9955 <kumarabhinay599@gmail.com>
2020-02-15 15:54:35 +0530

    added

--- app/src/main/java/com/example/schoolproject/Activities/TutorActivity.java
+++ app/src/main/java/com/example/schoolproject/Activities/TutorActivity.java
@@ -13,4 +13,13 @@ public class TutorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor);
     }
+
+    public static class AddChildActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add_child);
+        }
+    }
 }
