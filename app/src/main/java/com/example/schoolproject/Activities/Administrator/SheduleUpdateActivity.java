3c97f9f52f00747c9daeb6a377b099e3479e40e7
Nikhilranjan7352 <nikhilranjan7352@gmail.com>
2020-02-17 01:09:23 +0530

    hhh

---
+++ app/src/main/java/com/example/schoolproject/Activities/Administrator/SheduleUpdateActivity.java
@@ -0,0 +1,62 @@
+package com.example.schoolproject.Activities.Administrator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import com.example.schoolproject.Model.TutorModel;
+import com.example.schoolproject.R;
+import com.example.schoolproject.Utils.SheduleAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class SheduleUpdateActivity extends AppCompatActivity {
+
+    ArrayList<TutorModel> tutors;
+    ProgressDialog progressDialog;
+    RecyclerView recyclerView;
+    private SheduleAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shedule_update);
+        tutors=new ArrayList<>();
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        recyclerView=findViewById(R.id.suRecyclerview);
+        adapter=new SheduleAdapter(tutors,this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FirebaseDatabase.getInstance().getReference("Tutor").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                progressDialog.dismiss();
+                tutors.clear();
+                for(DataSnapshot ds:dataSnapshot.getChildren())
+                {
+                    TutorModel tm=new TutorModel(ds.child("email").getValue(String.class),ds.child("contact").getValue(String.class),ds.child("name").getValue(String.class),ds.child("tutorid").getValue(String.class),ds.getKey());
+                    tutors.add(tm);
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+              progressDialog.dismiss();
+            }
+        });
+    }
+}
