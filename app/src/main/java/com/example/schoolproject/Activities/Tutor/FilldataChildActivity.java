0f80e1a51695ed19370c1406d6410cb2a4722828
Nikhilranjan7352 <nikhilranjan7352@gmail.com>
2020-02-17 20:50:17 +0530

    hbssss

---
+++ app/src/main/java/com/example/schoolproject/Activities/Tutor/FilldataChildActivity.java
@@ -0,0 +1,16 @@
+package com.example.schoolproject.Activities.Tutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.schoolproject.R;
+
+public class FilldataChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filldata_child);
+    }
+}
